80781804206324e4e95d6ecdb7bfe48e4217e200
Lucas Siviglia <siviglialucas@gmail.com>
2025-03-18 14:29:58 +1000

    add time to email template

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -6,7 +6,7 @@ class BookingMailer < ApplicationMailer
     @booking = booking
     mail(
       to: @booking.user.email, # User's email
-      subject: "Booking Confirmation - #{@booking.service.name}"
+      subject: "New Booking - #{@booking.service.name}"
     )
   end
 
@@ -15,7 +15,7 @@ class BookingMailer < ApplicationMailer
     @booking = booking
     mail(
       to: 'siviglialucas@gmail.com', # Admin's email address
-      subject: "New Booking Created - #{@booking.service.name}"
+      subject: "Booking confirmation - #{@booking.service.name}"
     )
   end
 end
