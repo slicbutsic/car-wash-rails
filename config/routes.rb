2f7ed3af005950e8185956a3dbb5147c0186719a
Lucas Siviglia <siviglialucas@gmail.com>
2025-03-20 16:48:09 +1000

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
-  root to: "services#index"
+  root to: "dashboard#index"
 
   # Resources for services
   resources :services
