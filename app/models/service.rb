99ac37748d3cbcf5eb2fabe566683f8e1cbc3672
Lucas Siviglia <siviglialucas@gmail.com>
2025-03-20 16:18:46 +1000

    add style to booking

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,4 +5,18 @@ class Service < ApplicationRecord
 
   validates :name, presence: true
   validates :duration, presence: true
+
+
+  def formatted_duration
+    if duration.present?
+      hours = duration / 60
+      minutes = duration % 60
+      if duration >= 60
+        "#{hours}h #{minutes}min"
+      else
+        "#{duration} minutes"
+      end
+    end
+  end
+
 end
