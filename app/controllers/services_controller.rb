2ff8c4abfa74ec5970a3162138b9df6645bb38d4
Lucas Siviglia <siviglialucas@gmail.com>
2025-04-11 10:05:51 +1000

    delete service admin account

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,6 +5,25 @@ class ServicesController < ApplicationController
     @services = Service.includes(:prices).all
   end
 
+  def new
+    @service = Service.new
+    @vehicle_types = VehicleType.all
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      # You might want to initialize prices for each vehicle type here
+      VehicleType.all.each do |vt|
+        @service.prices.create(vehicle_type: vt, price: 0.0)
+      end
+      redirect_to services_path, notice: 'Service created successfully.'
+    else
+      @vehicle_types = VehicleType.all
+      render :new
+    end
+  end
+
   def edit
     @service = Service.find(params[:id])
     @vehicle_types = VehicleType.all
@@ -13,15 +32,20 @@ class ServicesController < ApplicationController
   def update
     @service = Service.find(params[:id])
 
-    # Begin transaction to ensure both service and price updates happen atomically
     ActiveRecord::Base.transaction do
       if @service.update(service_params)
-        update_prices(@service) # Update prices based on new service data
+        update_prices(@service)
         redirect_to services_path, notice: 'Service updated successfully.'
       else
         render :edit
       end
     end
+  end
+
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+    redirect_to services_path, notice: 'Service deleted successfully.'
   end
 
   private
@@ -31,7 +55,6 @@ class ServicesController < ApplicationController
   end
 
   def authenticate_admin!
-    # Add your logic here to authenticate that the user is an admin
     unless current_user && current_user.email == "admin@gmail.com"
       redirect_to root_path, alert: "You are not authorised to access this page."
     end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  has_many :prices
+  has_many :prices, dependent: :destroy
   has_many :vehicle_types, through: :prices
 
   validates :name, presence: true
